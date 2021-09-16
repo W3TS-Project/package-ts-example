9094ee043a9cef7b26de7dd500b243749c8d4b60
WriteCoin <sovtsardom@gmail.com>
2021-09-16 22:27:04 +0300

    Revert "основной функционал добавлен"
    This reverts commit 22ea8051dd1aba678cd96527d435f836594bb9aa.

--- src/decl.d.ts
+++
@@ -1,7 +0,0 @@
-declare type integer = number
-declare type real = number
-declare type Primitive = boolean | integer | real | string
-declare type rawcode = integer | string
-
-declare type code = () => void
-declare type codeboolexpr = () => boolean
